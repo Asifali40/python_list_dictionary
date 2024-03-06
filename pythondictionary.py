4851c6471301c136f639972a8a986efbfa67a8e3
Asifali40 <162404747+Asifali40@users.noreply.github.com>
2024-03-06 09:44:11 +0500

    Create pythondictionary.py

---
+++ pythondictionary.py
@@ -0,0 +1,78 @@
+# Create a dictionat and print its elements
+
+Dict = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+print("\nDictionary with the use of Integer Keys: ")
+print(Dict)
+
+Dict = {'Name': 'Geeks', 1: [1, 2, 3, 4]}
+print("\nDictionary with the use of Mixed Keys: ")
+print(Dict)
+
+# Other way to create a dictionary
+
+Dict = {}
+print("Empty Dictionary: ")
+print(Dict)
+
+Dict = dict({1: 'Geeks', 2: 'For', 3: 'Geeks'})
+print("\nDictionary with the use of dict(): ")
+print(Dict)
+
+Dict = dict([(1, 'Geeks'), (2, 'For')])
+print("\nDictionary with each item as a pair: ")
+print(Dict)
+
+
+# Add Elements to a dictionary
+Dict = {}
+print("Empty Dictionary: ")
+print(Dict)
+Dict[0] = 'Geeks'
+Dict[2] = 'For'
+Dict[3] = 1
+print("\nDictionary after adding 3 elements: ")
+print(Dict)
+
+Dict['Value_set'] = 2, 3, 4
+print("\nDictionary after adding 3 elements: ")
+print(Dict)
+
+Dict[2] = 'Welcome'
+print("\nUpdated key value: ")
+print(Dict)
+Dict[5] = {'Nested': {'1': 'Life', '2': 'Geeks'}}
+print("\nAdding a Nested Key: ")
+print(Dict)
+
+
+# Access a value in dictionary
+Dict = {1: 'Geeks', 'name': 'For', 3: 'Geeks'}
+print("Accessing a element using key:")
+print(Dict['name'])
+print("Accessing a element using key:")
+print(Dict[1])
+
+
+# Access elements using get function
+Dict = {1: 'Geeks', 'name': 'For', 3: 'Geeks'}
+
+print("Accessing a element using get:")
+print(Dict.get(3))
+
+
+# Clear Pop and delete functions in dictionary
+dict1 = {1: "Python", 2: "Java", 3: "Ruby", 4: "Scala"}
+dict2 = dict1.copy()
+print(dict2)
+dict1.clear()
+print(dict1)
+print(dict2.get(1))
+print(dict2.items())
+print(dict2.keys())
+dict2.pop(4)
+print(dict2)
+dict2.popitem()
+print(dict2)
+dict2.update({3: "Scala"})
+print(dict2)
+print(dict2.values())
